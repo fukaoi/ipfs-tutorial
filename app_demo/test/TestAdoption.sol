610463f765b74390e727dec05125458de1bcd1e2
fukaoi <goodakira7@gmail.com>
2018-02-28 16:28:05 +0900

    added test

--- app_demo/test/TestAdoption.sol
+++ app_demo/test/TestAdoption.sol
@@ -3,3 +3,12 @@ pragma solidity ^0.4.18;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+  Adoption adoption = Adoption(DeplyedAddresses.Adoption);
+
+  function testUserCanAdoptPet() {
+    uint returnedId = adoption.adopt(8);
+    uint expected = 8;
+  }
+}
