5fd30d913cdfb1757998eb1392f14c8f44005315
fukaoi <goodakira7@gmail.com>
2018-03-04 22:54:37 +0900

    refactoring split normal version and contract version

--- app/src/js/app_contract.js
+++ app/src/js/app_contract.js
@@ -3,10 +3,31 @@
 // before ipfs daemon startup, Should ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 
 App = {
+  web3Provider: null,
+  contracts: {},
   ipfsApi: {},
 
   init: function () {
     App.ipfsApi = window.IpfsApi('localhost', '5001')
+    return App.initWeb3();
+  },
+
+  initWeb3: function () {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      App.web3Provider = new Web3.providers.HttpPrivider('http://localhost:9545');
+    }
+    web3 = new Web3(App.web3Provider);
+    return App.initContract();
+  },
+
+  initContract: function () {
+    $.getJSON('Adoption.json', function (data) {
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption.setProvider(App.web3Provider);
+    });
     return App.bindEvents();
   },
 
