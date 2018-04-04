21f8f93226b2ee3a1d3a22fd1f6b5b2d36e29804
fukaoi <info@fukaoi.org>
2018-04-04 12:19:03 +0900

    modified

--- app/contracts/Ipfs.sol
+++ app/contracts/Ipfs.sol
@@ -1,15 +1,24 @@
 pragma solidity ^0.4.18;
 
 contract Ipfs {
-    string fileName;
-
-
-    function setUploadFileInfo(string name) public {
-        require(bytes(name).length >= 0);
-        fileName = name;
+    struct FileInfos {
+        string name;
+        string url;
     }
 
-    function getUploadFileInfo() public returns(string) {
-        return fileName;
+    mapping (address => FileInfos) fileinfos;
+    address[] public files;
+
+    function setUploadFileInfo(address _address, string _name, string _url) public {
+        // require(bytes(name).length >= 0);
+        var fileinfo = fileinfos[_address];
+        fileinfo.name = _name;
+        fileinfo.url = _url;
+
+        files.push(_address) - 1;
+    }
+
+    function getUploadFileInfos() public returns(address[]) {
+        return files;
     }
 }
