89e43c7949ca09e34b0602a4d945b650e0cc4f4d
fukaoi <goodakira7@gmail.com>
2018-04-06 13:36:05 +0900

    modified

--- app/test/TestIpfs.sol
+++ app/test/TestIpfs.sol
@@ -8,8 +8,7 @@ contract TestIpfs {
   function testGet() public {
     // Ipfs ipfs = Ipfs(DeployedAddresses.Ipfs());
     // ipfs.getUploadFileInfos();
-    uint returnedId =
-     1;
+    uint returnedId = 1;
     uint expected = 1;
     Assert.equal(returnedId, expected, "Adoption of pet ID 8 be recorded");
   }
