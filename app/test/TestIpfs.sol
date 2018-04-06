f9a21c3a88343660ac58d4555a483c2a49781f1b
fukaoi <goodakira7@gmail.com>
2018-04-06 13:28:46 +0900

    modified

--- app/test/TestIpfs.sol
+++ app/test/TestIpfs.sol
@@ -6,18 +6,10 @@ import "../contracts/Ipfs.sol";
 
 contract TestIpfs {
   function testGet() public {
-    Ipfs ipfs = Ipfs(DeployedAddresses.Ipfs());
-    ipfs.getUploadFileInfos();
+    // Ipfs ipfs = Ipfs(DeployedAddresses.Ipfs());
+    // ipfs.getUploadFileInfos();
     uint returnedId = 1;
     uint expected = 1;
     Assert.equal(returnedId, expected, "Adoption of pet ID 8 be recorded");
-  }
-
-  function testA() public {
-    Ipfs ipfs = Ipfs(DeployedAddresses.Ipfs());
-    ipfs.getUploadFileInfos();
-    bool a = true;
-    bool b = false;
-    Assert.equal(a, b, "test");
   }
 }
