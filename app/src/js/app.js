df71e4dd3eb053f43be4e9ec8cc1fb620184d891
fukaoi <goodakira7@gmail.com>
2018-04-11 07:59:58 +0900

    modified

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -4,10 +4,11 @@
 
 //todo: regist type 
 // timestamp, filename     
-
+// var web3 = require('web3');
 App = {
   ipfsApi: {},
   web3: {},
+  contracts: {},
 
   init: function () {
     App.ipfsApi = window.IpfsApi('localhost', '5001');
@@ -18,6 +19,14 @@ App = {
       web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
     }
     web3 = new Web3(web3Provider);
+    console.log(web3);
+    $.getJSON('Ipfs.json', function (data) {
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption.setProvider(App.web3Provider);
+      console.log(App.contracts.Adoption);
+    });
+    return App.bindEvents();
     App.bindEvents();
   },
 
@@ -28,7 +37,6 @@ App = {
 
   handleSubmit: function (event) {
     console.log(web3.eth.accounts[0]);
-    web3.eth.contract
     event.preventDefault();
     const file = event.target[0].files[0];
     const reader = new window.FileReader();
