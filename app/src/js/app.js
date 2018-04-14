43ed66af4b4e94d3d7a893ea7ba879a094515d30
fukaoi <goodakira7@gmail.com>
2018-04-14 22:12:18 +0900

    add html template

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,28 +1,30 @@
-// https://ipfs.io/docs/api/
-// https://github.com/ipfs/js-ipfs-api
 // before ipfs daemon startup, Should ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 // before geth daemon startup, geth --dev --rpc --rpccorsdomain "*"'
 
 App = {
-  ipfsApi: {},
-  web3: {},
+  ipfs: {},
   contract: {},
 
   init: function () {
-    App.ipfsApi = window.IpfsApi('localhost', '5001');
+    App.initIpfs();
+    App.initContract('0x83a4bd51282b57c1418fb1641c451403df6aac78');
+    return App.bindEvents();
+  },
+
+  initIpfs: function() {
+    App.ipfs = window.IpfsApi('localhost', '5001');    
+  },
+
+  initContract: function (address) {
     var web3 = new Web3();
     $.getJSON('Ipfs.json', function (aritifact) { 
       web3.setProvider((new Web3.providers.HttpProvider('http://localhost:8545')));
       web3.eth.defaultAccount = web3.eth.coinbase;
-      var address = '0xc27904a7da65d0a7a16237a70f4a815214e646c8';
-
       App.contract = web3.eth.contract(aritifact.abi).at(address);
-      console.log(App.contract.getLength().c);
       for (var i = 0; i < App.contract.getLength(); i++) {
         console.log(App.contract.getUploadFileInfo(i));
       } 
     });
-    return App.bindEvents();
   },
 
   bindEvents: function () {
@@ -40,7 +42,7 @@ App = {
 
   saveIpfs: function (reader, filename) {
     const buf = buffer.Buffer(reader.result)
-    App.ipfsApi.files.add(buf, (err, result) => {
+    App.ipfs.files.add(buf, (err, result) => {
       if(err) {
         console.error(err);
         return;
@@ -50,11 +52,23 @@ App = {
       App.contract.setUploadFileInfo(filename, hash, { gas: 6000000, from: web3.eth.accounts[0] });
       $('#result').html(url);
     })
+  },
+
+  insertTemplate: function () {
+    var body = '';
+    for(var i = 0; i < 10; i++){
+      body += '<tr>';
+      body += `<td>${i}</td>`    
+      body += `<td>${i}</td>`    
+      body += '</tr>'
+    }
+    $('tbody').prepend(body);
   }
 };
 
 $(function () {
   $(window).load(function () {
     App.init();
+    App.insertTemplate();
   });
 });
